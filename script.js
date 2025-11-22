044a67bb09a8f9a2dd7ea417851ab3b6bdb50913
tSvtok <elhaddadkhadija13@gmail.com>
2025-11-22 17:16:01 +0100

    ajouter la logique de sav button

--- script.js
+++ script.js
@@ -18,16 +18,16 @@ function addExperience() {
         <h3 class="font-bold text-lg mb-2">Experience #${counterExp}</h3>
 
         <label class="block mb-1 font-semibold">Experience :</label>
-        <input type="text" name="experience[]" class="w-full p-2 border rounded mb-3" placeholder="Enter the experience" required>
+        <input type="text" class="experience w-full p-2 border rounded mb-3" placeholder="Enter the experience" required>
 
         <label class="block mb-1 font-semibold">Place:</label>
-        <input type="text" name="place[]" class="w-full p-2 border rounded mb-3" placeholder="Enter the place" required>
+        <input type="text" class="place w-full p-2 border rounded mb-3" placeholder="Enter the place" required>
 
         <label class="block mb-1 font-semibold">Start Date:</label>
-        <input type="date" name="start[]" class="w-full p-2 border rounded mb-3" required>
+        <input type="date" class="start w-full p-2 border rounded mb-3" required>
 
         <label class="block mb-1 font-semibold">End Date:</label>
-        <input type="date" name="end[]" class="w-full p-2 border rounded mb-3" required>
+        <input type="date" class="end w-full p-2 border rounded mb-3" required>
 
         <button type="button" class="mt-2 bg-red-500 text-white px-3 py-1 rounded removeBtn">
             Remove This Experience
@@ -67,4 +67,37 @@ let backBtn = document.getElementById("backBtn");
 backBtn.addEventListener('click', () =>{
     exp.classList.add("hidden");
     addPersn.classList.remove("hidden");  
+});
+
+
+saveBtn.addEventListener('click', () => {
+    const Name = document.getElementById("name").value;
+    const role = document.getElementById("role").value;
+    const phone = document.getElementById("phone").value;
+    const email = document.getElementById("email").value;
+
+     
+    const expWrappers = document.querySelectorAll("#experiences > div");
+    let experiences = [];
+
+    expWrappers.forEach(wrapper => {
+        let experience = wrapper.querySelector(".experience").value;
+        let place = wrapper.querySelector(".place").value;
+        let start = wrapper.querySelector(".start").value;
+        let end = wrapper.querySelector(".end").value;
+
+        experiences.push({ experience, place, start, end });
+    });
+
+    
+    let Staff = {
+        name: Name,
+        role: role,
+        phone: phone,
+        email: email,
+        experiences: experiences,
+    };
+
+    console.log(Staff);
+    alert("Staff data collected successfully!");
 });
